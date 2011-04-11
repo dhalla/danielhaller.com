1d2276319070ee2b33740d933673abe4471a71f1
dha <dha@bluemars.net>
2011-04-12 01:02:29 +0200

    Slider korrekt implementiert, Slidernavigation funktionsfaehig

--- js/main.js
+++ js/main.js
@@ -13,13 +13,24 @@ $(document).ready(function() {
     // Initialize anything slider
     // see options http://css-tricks.com/examples/AnythingSlider/
     $('#slider').anythingSlider({
-        buildArrows         : false, 
-        buildNavigation     : false,
+        // Appearance
+        width               : null,      // Override the default CSS width
+        height              : null,      // Override the default CSS height
+        easing              : "easeOutBounce",   
+
+        // Navigation
+        buildArrows         : false,      // If true, builds the forwards and backwards buttons
+        buildNavigation     : true,      // If true, builds a list of anchor links to link to each panel
+        enableNavigation    : true,      // if false, navigation links will still be visible, but not clickable.
+        appendControlsTo    : $('#navigation'),      // A HTML element (jQuery Object, selector or HTMLNode) to which the controls will be appended if not null
         autoPlay            : false,
-        easing              : "easeOutBounce"
+        navigationFormatter : function(index, panel){
+          return " Panel #" + index; // This would have each tab with the text 'Panel #X' where X = index
+        }        
     });
     
     // External Slider Navigation
+/*
     $('#about').click(function() {
         $('#slider').anythingSlider(1);
 
@@ -27,6 +38,7 @@ $(document).ready(function() {
     $('#more').click(function() {
         $('#slider').anythingSlider(2);
     });
+*/
 
 
 });
